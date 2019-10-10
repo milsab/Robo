1d1743ad8e9b5124e206dc11e7c4443d0d59dfd5
msabouri@hawk.iit.edu <milad.sabouri@gmail.com>
2019-10-10 18:48:06 -0500

    setup the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+// import { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robos: []
+    };
+  }
+
+  // ES2017 Promise Syntax
+  async componentDidMount() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    this.setState({ robos: users });
+  }
+
+  // ES2015 Promise Syntax
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then(response => {
+  //       console.log(response);
+  //       return response.json();
+  //     })
+  //     .then(users => this.setState({ monsters: users }));
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="search" placeholder="search Robos" onChange="" />
+        <CardList robos={this.state.robos} />
+      </div>
+    );
+  }
 }
 
 export default App;
